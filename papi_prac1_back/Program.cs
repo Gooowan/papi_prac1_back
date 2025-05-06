dc08a25372623bc69f4125ffb3d891acb59b35d0
BUNJEE <vsolomatin@kse.org.ua>
2025-05-06 12:50:18 +0300

    done

--- papi_prac1_back/Program.cs
+++ papi_prac1_back/Program.cs
@@ -8,9 +8,21 @@ builder.Services.AddScoped<MessageManager>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseRouting();
 
 app.MapGet("/", () => "API is running");
 app.UseCors("AllowAll");
